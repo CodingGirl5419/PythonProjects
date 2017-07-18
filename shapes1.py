d32ac21d2a09e2ef66206fe2d138372351162af6
CodingGirl5419 <alexandrab910@gmail.com>
2017-07-18 16:02:48 -0400

    Shapes with Python

---
+++ shapes1.py
@@ -0,0 +1,28 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(500,300)
+#x_pos = -250
+#y_pos = -150
+#
+### Write your code below:
+pendown()
+pencolor("red")
+forward (90)
+pencolor("purple")
+left (60)
+pencolor("green")
+back (90)
+pencolor("pink")
+left (60)
+pencolor("yellow")
+forward (90)
+pencolor("brown")
+
+
+# Close window on click.
+exitonclick()
