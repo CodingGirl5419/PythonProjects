b0bcb760e093e28d2dacfd34f6f88ae4611c2b5d
CodingGirl5419 <alexandrab910@gmail.com>
2017-07-21 15:16:25 -0400

    Create listsfood.py

---
+++ listsfood.py
@@ -0,0 +1,10 @@
+import random
+
+food = ["dinner", "dessert", "sides"]
+dinner = ["pasta", "sushi", "tacos"]
+dessert = ["ice cream", "cookies", "donuts"]
+sides = ["fries", "salad", "fruit"]
+
+print ("Your dinner is " + random.choice(dinner))
+print (random.choice(dessert))
+print (random.choice(sides))
