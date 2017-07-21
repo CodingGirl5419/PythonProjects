8c04be785aa70d80f3ad216e31b23bb5f7a3a877
CodingGirl5419 <alexandrab910@gmail.com>
2017-07-21 15:16:49 -0400

    Create listshaiku.py

---
+++ listshaiku.py
@@ -0,0 +1,9 @@
+import random
+
+firstLines = ["The cat caught the mouse", "The dog chewed his bone" ]
+secondLines = ["The mouse tried to escape", "The dog decided to drink water"]
+thirdLines = ["The mouse was soon freed", "The dog slept at night"]
+
+print(random.choice(firstLines))
+print(random.choice(secondLines))
+print(random.choice(thirdLines))
