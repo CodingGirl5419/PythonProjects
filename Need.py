d9b99dbea392e1c1024e3d0a0e8055549694ce8f
CodingGirl5419 <alexandrab910@gmail.com>
2017-11-05 13:45:18 -0500

    Create Need.py

---
+++ Need.py
@@ -0,0 +1,14 @@
+#print("-----------------")
+#input("What do you need?")
+#print("-----------------")
+#print("Your need is pending...")
+#print("...")
+#print("...")
+#print("...")
+#print("Your need has been approved!")
+#print("Please do as following...")
+#print("Invite 5 new members")
+
+myList = [("apples", "pineapples", "peaches"), ("spinach", "carrots", "kale")]
+print (myList[0][0])
+print (myList[1][0])
